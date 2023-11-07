832cc692db23cd8903514ccc94bcba866d035de4
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-07 10:40:28 -0300

    .

--- tp10_portfolio/src/components/Card.js
+++ tp10_portfolio/src/components/Card.js
@@ -1,10 +1,10 @@
 import React, {useContext} from 'react'
 import { FavoritosContext } from '../context/FavoritosContext'
-<<<<<<< HEAD
-import { BiHeart } from 'react-icons/bi'
-=======
 
->>>>>>> ca1c3e720689becd312ed4db9f538bb8c01bbf6a
+import { BiHeart } from 'react-icons/bi'
+
+
+
 
 export default function Card({post}) {
   const { AddFavorito }= useContext(FavoritosContext);
