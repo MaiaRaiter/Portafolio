d7b50219d951d89a7c92445f5f2e412719cf2a5c
SHIRLY JUSID <46875521@ort>
2023-11-09 09:40:51 -0300

    footer

--- tp10_portfolio/src/components/Footer.js
+++ tp10_portfolio/src/components/Footer.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import Contacto from '../components/Contacto.js'
-export default function Footer ()  {
+import { RxInstagramLogo } from "react-icons/rx";
+import { RxTwitterLogo } from "react-icons/rx";
+import { RxLinkedinLogo } from "react-icons/rx";
+
+
+
+export default function Footer() {
   return (
-    <head>
-        < Contacto />
-    </head>
+    <footer>
+
+      <div className="container">
+        <footer>
+
+          <div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div className="col-md-4 d-flex flex-column align-items-center">
+              <span className="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+
+              <div className="footer-wrapper_branding-social">
+                <ul className="ms-3"><RxTwitterLogo className='redes-socifooter-wrapper_branding-social' href='https://twitter.com/' /> </ul>
+                <ul className="ms-3"><RxInstagramLogo className='redes-socifooter-wrapper_branding-social' href='https://www.instagram.com/' /> </ul>
+                <ul className="ms-3"><RxLinkedinLogo className='redes-socifooter-wrapper_branding-social' href='https://www.linkedin.com/' /> </ul>
+              </div>
+
+            </div>
+            <div className="col-md-8 d-flex align-items-center">
+              < Contacto />
+            </div>
+          </div>
+        </footer>
+      </div>
+    </footer>
   )
 }
--- tp10_portfolio/src/components/Header.js
+++ tp10_portfolio/src/components/Header.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, {useContext } from 'react'
 import { Link } from "react-router-dom";
+import { FavoritosContext } from '../context/FavoritosContext'
 
-export default function Header ()  {
+export default function Header() {
+  const { cantidadFavoritos } = useContext(FavoritosContext);
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white py-3">
       <div className="container px-5">
-          
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
-              <Link to="/"> <li className="nav-item"><a className="nav-link" >Home</a></li></Link>
-              <Link to="/InfoPersonal"> <li className="nav-item"><a className="nav-link" >Info</a></li></Link>
-              <Link to="/MisCreaciones"> <li className="nav-item"><a className="nav-link" >Mis Creaciones</a></li></Link>
-              <Link to="/Favoritos"> <li className="nav-item"><a className="nav-link" >Favoritos</a></li></Link>
-              </ul>
-          </div>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+            <Link to="/"> <li className="nav-item"><a className="nav-link" >Home</a></li></Link>
+            <Link to="/InfoPersonal"> <li className="nav-item"><a className="nav-link" >Info</a></li></Link>
+            <Link to="/MisCreaciones"> <li className="nav-item"><a className="nav-link" >Mis Creaciones</a></li></Link>
+            <Link to="/Favoritos"> <li className="nav-item"><a className="nav-link" >Favoritos <span class="badge text-bg-secondary">{cantidadFavoritos}</span></a></li></Link>
+            
+          </ul>
+        </div>
       </div>
     </nav>
   )
